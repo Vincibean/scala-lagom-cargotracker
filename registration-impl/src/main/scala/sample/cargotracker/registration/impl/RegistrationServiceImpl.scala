e8c68c363aaea3229d79fa824f06b7476c42538a
Vincibean <andrebessi00@gmail.com>
2017-07-20 16:52:39 +0200

    Provided implementation for RegistrationServiceImpl

--- registration-impl/src/main/scala/sample/cargotracker/registration/impl/RegistrationServiceImpl.scala
+++ registration-impl/src/main/scala/sample/cargotracker/registration/impl/RegistrationServiceImpl.scala
@@ -1,21 +1,51 @@
 package sample.cargotracker.registration.impl
 
-import akka.{Done, NotUsed}
 import akka.stream.scaladsl.Source
-import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
+import akka.{Done, NotUsed}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.persistence.cassandra.{CassandraReadSide, CassandraSession}
-import com.lightbend.lagom.scaladsl.pubsub.PubSubRegistry
+import com.lightbend.lagom.scaladsl.persistence.{PersistentEntityRef, PersistentEntityRegistry}
+import com.lightbend.lagom.scaladsl.pubsub.{PubSubRef, PubSubRegistry, TopicId}
+import org.slf4j.{Logger, LoggerFactory}
 import sample.cargotracker.registration.api.{Cargo, RegistrationService}
+
+import scala.concurrent.Future
 
 class RegistrationServiceImpl(topics: PubSubRegistry, persistentEntityRegistry: PersistentEntityRegistry,
                               readSide: CassandraReadSide, db: CassandraSession) extends RegistrationService {
 
-  override def register(): ServiceCall[Cargo, Done] = ???
+  private[this] val log: Logger = LoggerFactory.getLogger(this.getClass)
 
-  override def getLiveRegistrations(): ServiceCall[NotUsed, Source[Cargo, NotUsed]] = ???
+  override def register(): ServiceCall[Cargo, Done] = ServiceCall { request =>
+    val topic: PubSubRef[Cargo] = topics.refFor(TopicId[Cargo]("topic"))
+    topic.publish(request)
+    log.info("Cargo ID: {}.", request.id)
+    val ref: PersistentEntityRef[RegistrationCommand] = persistentEntityRegistry.refFor[CargoEntity](request.id)
+    ref.ask(RegisterCargo(request))
+  }
 
-  override def getAllRegistrations(): ServiceCall[NotUsed, Seq[Cargo]] = ???
+  override def getLiveRegistrations(): ServiceCall[NotUsed, Source[Cargo, NotUsed]] = ServiceCall { request =>
+    val topic: PubSubRef[Cargo] = topics.refFor(TopicId[Cargo]("topic"))
+    Future.successful(topic.subscriber)
+  }
 
-  override def getRegistration(): ServiceCall[String, Cargo] = ???
+  override def getAllRegistrations(): ServiceCall[NotUsed, Seq[Cargo]] = ServiceCall { _ =>
+    db.selectAll("SELECT cargoid, name, description, owner, destination FROM cargo").map { rows =>
+      rows.map { r =>
+        Cargo(
+          r.getString("cargoid"),
+          r.getString("name"),
+          r.getString("description"),
+          r.getString("owner"),
+          r.getString("destination")
+        )
+      }
+    }
+  }
+
+  override def getRegistration(): ServiceCall[String, Cargo] = ServiceCall { request =>
+    //TODO Implement meaningful
+    ???
+  }
+
 }
